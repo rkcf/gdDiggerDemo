d12104621fdade5a76bfcb871e60fb9aef5ad111
rkcf <rkcf@rkcf.me>
2022-04-14 12:30:09 -0500

    add zoom in/out functionality

---
+++ scenes/Camera.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_released("ui_page_up"):
+		self.zoom.x -= 0.1
+		self.zoom.y -= 0.1	
+	if Input.is_action_just_released("ui_page_down"):
+		self.zoom.x += 0.1
+		self.zoom.y += 0.1
+		
