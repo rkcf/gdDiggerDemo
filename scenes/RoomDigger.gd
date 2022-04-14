0e8c4cd82208fe7514de326f0723158f19bbee99
rkcf <rkcf@rkcf.me>
2022-04-14 09:00:04 -0500

    add assert and make sure starting position is more in boundary, still issue tho

--- scenes/RoomDigger.gd
+++ scenes/RoomDigger.gd
@@ -20,6 +20,7 @@ func create_room() -> Room2D:
 	var height: int = round(rand_range(min_room_size, max_room_size))
 	
 	# TODO Fix this, make sure rooms are always positioned within the boundary
+	print(position)
 	assert(boundary.has_point(position))
 	
 	# We failed to randomly pick a point in space, so we are going to step down from the max size until we find one that works
@@ -44,8 +45,7 @@ func create_room() -> Room2D:
 	# Create the size vector
 	var size: Vector2 = Vector2(width, height)
 	# Find the top left corner to store the position
-	var top_corner: Vector2 = (position - size/2).ceil()
-	var new_room: Room2D = Room2D.new(top_corner, size)
+	var new_room: Room2D = Room2D.new(position, size)
 	return new_room
 
 
