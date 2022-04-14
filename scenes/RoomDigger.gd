4cfb5ab0ba2b2e19b3886004d9ce666d4d2dd506
rkcf <rkcf@rkcf.me>
2022-04-14 08:52:01 -0500

    fix bug where room wanted to go outside level boundary

--- scenes/RoomDigger.gd
+++ scenes/RoomDigger.gd
@@ -19,6 +19,24 @@ func create_room() -> Room2D:
 	var width: int = round(rand_range(min_room_size, max_room_size))
 	var height: int = round(rand_range(min_room_size, max_room_size))
 	
+	# TODO Fix this, make sure rooms are always positioned within the boundary
+	assert(boundary.has_point(position))
+	
+	# We failed to randomly pick a point in space, so we are going to step down from the max size until we find one that works
+	width = max_room_size
+	while not boundary.has_point(Vector2(position.x + width, position.y)):
+		width -= 1
+		# abandon making the room if it is too small
+		if width < 1:
+			break
+	
+	height = max_room_size
+	while not boundary.has_point(Vector2(position.x, position.y + height)):
+		height -= 1
+		# abandon making the room if it is too small
+		if height < 1:
+			break
+	
 	# Make sure width and height are within boundary
 	assert(boundary.has_point(Vector2(position.x + width, position.y)))
 	assert(boundary.has_point(Vector2(position.x, position.y + height)))
