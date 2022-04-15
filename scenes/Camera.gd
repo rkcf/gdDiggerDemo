17776f0a9e5681592ad643521949e6234449134a
rkcf <rkcf@rkcf.me>
2022-04-15 07:44:53 -0500

    add keyboard movement to camera

--- scenes/Camera.gd
+++ scenes/Camera.gd
@@ -8,3 +8,12 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_released("ui_page_down"):
 		self.zoom.x += 0.1
 		self.zoom.y += 0.1
+	
+	if Input.is_action_pressed("ui_left"):
+		self.position.x -= 100	
+	if Input.is_action_pressed("ui_right"):
+		self.position.x += 100	
+	if Input.is_action_pressed("ui_up"):
+		self.position.y -= 100	
+	if Input.is_action_pressed("ui_down"):
+		self.position.y += 100
